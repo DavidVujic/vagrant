ac50d4d508fac556ed36716cb64a72ad46d29880
David Vujic <david@vujic.se>
2015-01-26 14:02:04 +0100

    Fixed character encoding issues

--- scripts/Modify-Firewall.ps1
+++ scripts/Modify-Firewall.ps1
@@ -1,1 +1,1 @@
-New-NetFirewallRule -DisplayName "SQL Server DEV" -Direction Inbound –Protocol TCP –LocalPort 1433 -Action allow
+New-NetFirewallRule -DisplayName "SQL Server DEV" -Direction Inbound -Protocol TCP -LocalPort 1433 -Action allow
