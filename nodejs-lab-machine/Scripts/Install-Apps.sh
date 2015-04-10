ed777c4ab28625724a7febc9090d64d759fd4bb0
David Vujic <david@vujic.se>
2015-04-10 22:42:50 +0200

    added npm and node link

--- nodejs-lab-machine/Scripts/Install-Apps.sh
+++ nodejs-lab-machine/Scripts/Install-Apps.sh
@@ -1,7 +1,9 @@
 sudo apt-get update
 
 curl -sL https://deb.nodesource.com/setup | sudo bash -
-sudo apt-get install -y nodejs
+sudo apt-get install -y nodejs npm
+
+sudo ln -s /usr/bin/nodejs /usr/bin/node
 
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
