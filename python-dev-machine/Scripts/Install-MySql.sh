53055387f650794ea98436afe4ed760366344699
David Vujic <david@vujic.se>
2015-10-13 10:54:40 +0200

    Added install script for MySQL

---
+++ python-dev-machine/Scripts/Install-MySql.sh
@@ -0,0 +1,9 @@
+echo '**********'
+echo 'running Install-MySql script'
+echo '**********'
+
+echo "mysql-server mysql-server/root_password password vagrant" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password vagrant" | debconf-set-selections
+
+sudo apt-get -y install mysql-server
+sudo apt-get install -y mysql-client
